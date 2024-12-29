dcbb8810f767708e39570e6838229ad65470598a
Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
2024-12-29 19:15:18 +0100

    Build: Bump fmt dependency to 11.2.0, and spdlog to 1.15.3

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class MikageConan(ConanFile):
     requires = [
         #"boost/1.79.0",
         "boost/1.84.0",
-        "spdlog/1.10.0",
+        "spdlog/1.15.3",
         "cryptopp/8.5.0",
         "sdl/2.30.7", # 2.0.18 fixed swapped X/Y buttons on Switch Pro Controller
         "range-v3/0.12.0",
@@ -17,7 +17,7 @@ class MikageConan(ConanFile):
         "spirv-tools/1.3.268.0",
         "tracy/0.11.1",
         "xxhash/0.8.0",
-        "fmt/8.1.1",
+        "fmt/11.2.0",
     ]
 
     options = {
